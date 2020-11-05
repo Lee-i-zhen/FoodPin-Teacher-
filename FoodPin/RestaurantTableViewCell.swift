61229dad3042bbacc454a3cd3e92ca4eca81717c
lee-i-zhen <410721302@gms.ndhu.edu.tw>
2020-11-05 10:56:08 +0800

    11/5  轉場

--- FoodPin/RestaurantTableViewCell.swift
+++ FoodPin/RestaurantTableViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class RestaurantTableViewCell: UITableViewCell {
     
+    
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var locationLabel: UILabel!
     @IBOutlet var typeLabel: UILabel!
